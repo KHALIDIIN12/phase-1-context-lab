5af79bdba3b28424bc3108c4855fecb1a35a8f7a
KHALIDIIN12 <khalidinho53@gmail.com>
2025-06-27 14:09:43 +0300

    Complete Payroll Time-Card Lab

--- index.js
+++ index.js
@@ -1,23 +1,100 @@
-/* Your Code Here */
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
-
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
-
-const allWagesFor = function () {
-    const eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
-
-    const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
-
-    return payable
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+  return {
+    firstName,
+    familyName,
+    title,
+    payPerHour,
+    timeInEvents: [],
+    timeOutEvents: [],
+  };
 }
 
+function createEmployeeRecords(records) {
+  return records.map(createEmployeeRecord);
+}
+
+
+function createTimeInEvent(dateStamp) {
+  const [date, hour] = dateStamp.split(" ");
+  this.timeInEvents.push({
+    type: "TimeIn",
+    hour: parseInt(hour, 10),
+    date,
+  });
+  return this;
+}
+
+function createTimeOutEvent(dateStamp) {
+  const [date, hour] = dateStamp.split(" ");
+  this.timeOutEvents.push({
+    type: "TimeOut",
+    hour: parseInt(hour, 10),
+    date,
+  });
+  return this;
+}
+
+function hoursWorkedOnDate(date) {
+  const timeIn = this.timeInEvents.find((e) => e.date === date);
+  const timeOut = this.timeOutEvents.find((e) => e.date === date);
+  return (timeOut.hour - timeIn.hour) / 100;
+}
+
+function wagesEarnedOnDate(date) {
+  return hoursWorkedOnDate.call(this, date) * this.payPerHour;
+}
+
+
+function allWagesFor() {
+  const dates = this.timeInEvents.map((e) => e.date);
+  return dates.reduce(
+    (total, d) => total + wagesEarnedOnDate.call(this, d),
+    0
+  );
+}
+
+
+function findEmployeeByFirstName(srcArray, firstName) {
+  return srcArray.find((rec) => rec.firstName === firstName);
+}
+
+function calculatePayroll(employeeRecords) {
+  return employeeRecords.reduce(
+    (total, rec) => total + allWagesFor.call(rec),
+    0
+  );
+}
+
+
+module.exports = {
+  createEmployeeRecord,
+  createEmployeeRecords,
+  createTimeInEvent,
+  createTimeOutEvent,
+  hoursWorkedOnDate,
+  wagesEarnedOnDate,
+  allWagesFor,
+  findEmployeeByFirstName,
+  calculatePayroll,
+};
+
+
+if (require.main === module) {
+  const emps = createEmployeeRecords([
+    ["Gray", "Worm", "Security", 10],
+    ["Daenerys", "Targaryen", "CEO", 100],
+  ]);
+
+
+  createTimeInEvent.call(emps[0], "2025-06-27 0900");
+  createTimeOutEvent.call(emps[0], "2025-06-27 1700");
+
+
+  createTimeInEvent.call(emps[1], "2025-06-27 1000");
+  createTimeOutEvent.call(emps[1], "2025-06-27 1600");
+
+  console.log("Gray Worm wages:", allWagesFor.call(emps[0])); 
+  console.log("Dany wages:", allWagesFor.call(emps[1]));      
+  console.log("Total payroll:", calculatePayroll(emps));     
+}
